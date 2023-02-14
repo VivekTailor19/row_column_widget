1c68fa69d73bab2f2b8313a89a9ab510c5799592
Vivek <tailorvivekgec19@gmail.com>
2023-02-13 22:23:12 -0800

    row_column_widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,16 @@ import 'package:row_column_widget/row_column_04/row_column_04.dart';
 import 'package:row_column_widget/row_column_05/row_column_05.dart';
 import 'package:row_column_widget/row_column_06/row_column_06.dart';
 
+import 'vivek.dart';
+
+
+
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RowColumn06(),
-    ),
+      home: RowColumn06()
+    )
   );
 }
--- lib/row_column_06/row_column_06.dart
+++ lib/row_column_06/row_column_06.dart
@@ -14,20 +14,198 @@ class _RowColumn06State extends State<RowColumn06> {
       body: Column(
         children: [
           Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(child: Container(height: 100,width: 200,color: Colors.black,)),
-                  Expanded(child: Container(height: 100,width: 200,color: Colors.white,)),
-                ],
-              ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child:
+                  Column( crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(child: Container(height: 100, width: 200, color: Colors.yellow,child: Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(child: Container(height: 100,width: 200,color: Colors.red,child: Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Expanded(child: Container(height: 100,width: 200,color: Colors.green)),
+                                  Expanded(child: Container(height: 100,width: 200,color: Colors.white,child: Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                                      children: [
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.orange)),
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.pinkAccent,child: Expanded(
+                                          child: Row(
+                                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                                            children: [
+                                              Expanded(child: Container(height: 100,width: 200,color: Colors.grey)),
+                                              Expanded(child: Container(height: 100,width: 200,color: Colors.black,child: Expanded(
+                                                child: Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                                                  children: [
+                                                    Expanded(child: Container(height: 100,width: 200,color: Colors.redAccent)),
+                                                    Expanded(child: Container(height: 100,width: 200,color: Colors.yellow,)),
+                                                  ],),
+                                              ),)),
+                                            ],),
+                                        ),)),
+                                      ],),
+                                  ),)),
+                                ],),
+                            ),)),
+                            Expanded(child: Container(height: 100,width: 200,color: Colors.white,child: Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Expanded(child: Container(height: 100,width: 200,color: Colors.grey,child: Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                                      children: [
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.white)),
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.red)),
+                                      ],),
+                                  ),)),
+                                  Expanded(child: Container(height: 100,width: 200,color: Colors.black,)),
+                                ],),
+                            ),)),
+                            Expanded(child: Container(height: 100,width: 200,color: Colors.deepPurpleAccent)),
+
+                          ],),
+                      ))),
+                    ],
+                  ),
+                ),
+                Expanded(child: Container(height: 200, width: 200, color: Colors.lightBlue,child: Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(child: Container(height: 100,width: 200,color: Colors.blueGrey,child: Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(child: Container(height: 100,width: 200,color: Colors.yellow,child: Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Expanded(child: Container(height: 100,width: 200,color: Colors.deepOrange,child: Expanded(
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                                      children: [
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.blueAccent)),
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.white,child: Expanded(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                                            children: [
+                                              Expanded(child: Container(height: 100,width: 200,color: Colors.deepPurple.shade900,child: Expanded(
+                                                child: Row(
+                                                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                                                  children: [
+                                                    Expanded(child: Container(height: 100,width: 200,color: Colors.black38)),
+                                                    Expanded(child: Container(height: 100,width: 200,color: Colors.white,)),
+                                                  ],),
+                                              ),)),
+                                              Expanded(child: Container(height: 100,width: 200,color: Colors.black,)),
+                                            ],),
+                                        ),)),
+                                      ],),
+                                  ),)),
+                                  Expanded(child: Container(height: 100,width: 200,color: Colors.red.shade50,)),
+                                ],),
+                            ),)),
+                            Expanded(child: Container(height: 100,width: 200,color: Colors.pink)),
+                          ],),
+                      ),)),
+                      Expanded(child: Container(height: 100,width: 200,color: Colors.blueGrey,child: Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(child: Container(height: 100,width: 200,color: Colors.blueAccent)),
+                            Expanded(child: Container(height: 100,width: 200,color: Colors.white,child: Expanded(
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Expanded(child: Container(height: 100,width: 200,color: Colors.indigoAccent.shade700,child: Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                                      children: [
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.black,child: Expanded(
+                                          child: Row(
+                                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                                            children: [
+                                              Expanded(child: Container(height: 100,width: 200,color: Colors.orange)),
+                                              Expanded(child: Container(height: 100,width: 200,color: Colors.red,)),
+                                            ],),
+                                        ),)),
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.brown,)),
+                                      ],),
+                                  ),)),
+                                  Expanded(child: Container(height: 100,width: 200,color: Colors.red.shade200)),
+                                ],),
+                            ),),
+                            ),],),
+                      ),)),
+                    ],),
+                ),)),
+              ],
             ),
+          ),
           Expanded(
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Expanded(child: Container(height: 100,width: 200,color: Colors.white,)),
-                Expanded(child: Container(height: 100,width: 200,color: Colors.black,)),
+                Expanded(child: Container(height: 100,width: 200,color: Colors.white,child: Expanded(
+                          child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                               Expanded(child: Container(height: 100,width: 200,color: Colors.blue)),
+                              Expanded(child: Container(height: 100,width: 200,color: Colors.lime,child: Expanded(
+                                  child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                                      children: [
+                                      Expanded(child: Container(height: 100,width: 200,color: Colors.orange)),
+                                      Expanded(child: Container(height: 100,width: 200,color: Colors.limeAccent)),
+                                      ],),
+                                      ),)),
+                          ],),
+                          ),)),
+                Expanded(child: Container(height: 100,width: 200,color: Colors.black,child: Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(child: Container(height: 100,width: 200,color: Colors.redAccent.shade100)),
+                      Expanded(child: Container(height: 100,width: 200,color: Colors.lightGreen.shade50,child: Expanded(
+                                  child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                                  children: [
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.deepPurpleAccent)),
+                                        Expanded(child: Container(height: 100,width: 200,color: Colors.deepPurpleAccent.shade100,
+                                          child: Expanded(
+                                            child: Row(
+                                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                                            children: [
+                                              Expanded(child: Container(height: 100,width: 200,color: Colors.pink,child: Expanded(
+                                                  child: Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                                                  children: [
+                                                      Expanded(child: Container(height: 100,width: 200,color: Colors.grey)),
+                                                      Expanded(child: Container(height: 100,width: 200,color: Colors.yellowAccent,child: Expanded(
+                                                        child: Row(
+                                                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                                                          children: [
+                                                            Expanded(child: Container(height: 100,width: 200,color: Colors.green.shade900)),
+                                                            Expanded(child: Container(height: 100,width: 200,color: Colors.black)),
+                                                          ],),
+                                                      ),)),
+                                                  ],),
+                                                  ),)),
+                                              Expanded(child: Container(height: 100,width: 200,color: Colors.red,)),
+                                            ],),
+                                        ),)),
+                                  ],),
+                                  ),)),
+                    ],),
+                ),)),
               ],
             ),
           ),
@@ -39,9 +217,37 @@ class _RowColumn06State extends State<RowColumn06> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Expanded(child: Container(height: 100,width: 200,color: Colors.grey)),
-                      Expanded(child: Container(height: 100,width: 200,color: Colors.red,)),
-                    ],),
+                      Expanded(child: Container(height: 100,width: 200,color: Colors.pink.shade300)),
+                      Expanded(child: Container(height: 100,width: 200,color: Colors.red,child: Expanded(
+                            child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                                Expanded(child: Container(height: 100,width: 200,color: Colors.yellowAccent)),
+                                Expanded(child: Container(height: 100,width: 200,color: Colors.white,child: Expanded(
+                                  child: Row(
+                                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                                    children: [
+                                      Expanded(child: Container(height: 100,width: 200,color: Colors.pink,child: Expanded(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                                          children: [
+                                            Expanded(child: Container(height: 100,width: 200,color: Colors.grey)),
+                                            Expanded(child: Container(height: 100,width: 200,color: Colors.yellowAccent,child: Expanded(
+                                              child: Row(
+                                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                                children: [
+                                                  Expanded(child: Container(height: 100,width: 200,color: Colors.green.shade900)),
+                                                  Expanded(child: Container(height: 100,width: 200,color: Colors.black)),
+                                                ],),
+                                            ),)),
+                                          ],),
+                                      ),)),
+                                      Expanded(child: Container(height: 100,width: 200,color: Colors.red,)),
+                                    ],),
+                                ),),)
+                            ],),
+                            ),)
+                      ),],),
                 ),)),
                 Expanded(child: Container(height: 100,width: 200,color: Colors.white,child: Expanded(
                   child: Row(
